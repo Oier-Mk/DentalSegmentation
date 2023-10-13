871b80912eb4ceb2e0521cb3a357ac7345672ca5
Danel Arias <71316377+DanelArias-Dreyton257@users.noreply.github.com>
2023-10-13 10:18:38 +0200

    CPP project

--- main.cpp
+++ main.cpp
@@ -1,7 +1,44 @@
-#include <stdio.h>
+#include <iostream>
+#include <dcmtk/dcmdata/dctk.h>
+#include <dcmtk/config/osconfig.h>
+
+void loadDicomFile(std::string filename)
+{
+    DcmFileFormat fileformat;
+    OFCondition status = fileformat.loadFile(filename.c_str());
+
+    if (status.good())
+    {
+        // File loaded successfully
+
+        // Access the DICOM dataset
+        DcmDataset *dataset = fileformat.getDataset();
+
+        // Print something related to the dataset to check if it is loaded correctly
+        OFString patientName;
+        if (dataset->findAndGetOFString(DCM_PatientName, patientName).good())
+        {
+            std::cout << "Patient Name: " << patientName << std::endl;
+        }
+        else
+        {
+            std::cerr << "Error: cannot access Patient Name!" << std::endl;
+        }
+
+        // TODO: Process the DICOM dataset as needed
+    }
+    else
+    {
+        // Error loading the file
+        std::cerr << "Error: Unable to load DICOM file " << filename << std::endl;
+    }
+}
+
 int main()
 {
-    // printf() displays the string inside quotation
-    printf("Hello, World!");
+    std::string filename = "data/Etiquetado_17682_20230302_152135/17682_20230302_152135_0.dcm";
+    loadDicomFile(filename);
+
+    std::cout << "Hello World!";
     return 0;
 }
--- python/models.py
+++ python/models.py
@@ -10,29 +10,28 @@ Side Note: These structures can also be used for
            need to apply accordingly
 '''
 
+
 class UNETModel(keras.Model):
     def __init__(self):
         super(UNETModel, self).__init__()
-
-        
+    
     def build(self, input_shape):
         
         # The encoder has 4 blocks of 2 conv3d layers
         # The input shape is 764x764x603
         self.encoder = keras.Sequential([
-            #TODO
+            # TODO
         ])
         # The middle is a bottleneck of 2 conv3d layers
         self.middle = keras.Sequential([
-            #TODO
+            # TODO
         ])
 
         # The decoder has 4 blocks of 2 conv3d layers
         # The output shape is a mask of 764x764x603
         self.decoder = keras.Sequential([
-            #TODO
+            # TODO
         ])
-
 
     def __call__(self, inputs):
         x1 = self.encoder(inputs)
